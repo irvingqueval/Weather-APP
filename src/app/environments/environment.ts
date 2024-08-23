5a4824bb229d225b17f6848fd04ba8be28ea9050
Irving Queval <irvingqueval@gmail.com>
2024-08-23 11:13:53 +0200

    files added

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+
+  apiKey: 'YOUR API KEY HERE',
+  apuUrl: 'https://api.openweathermap.org/data/2.5/weather',
+};
---
+++ src/app/services/weather-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+
+
+const API_KEY = environment.apiKey;
+const API_URL = environment.apuUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherApiService {
+  private http = inject(HttpClient);
+
+  getWeather(city: string) {
+    return this.http.get(`${API_URL}?q=${city}&appid=${API_KEY}`);
+  }
+}
